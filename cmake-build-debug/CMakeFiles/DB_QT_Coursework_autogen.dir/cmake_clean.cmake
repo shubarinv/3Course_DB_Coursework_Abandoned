84a25bf4c025e6e7d94c1b2fd67fe768d46104ae
Vladimir Shubarin <vhundef@gmail.com>
2021-03-02 16:56:32 +0300

    removed cmake_debug folder

--- cmake-build-debug/CMakeFiles/DB_QT_Coursework_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/DB_QT_Coursework_autogen"
-  "DB_QT_Coursework_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/DB_QT_Coursework_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
