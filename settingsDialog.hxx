0cbee5b9fab84d10d389613ab392d13ef0157dd9
Vladimir Shubarin <vhundef@gmail.com>
2021-03-02 21:03:04 +0300

    Will now save/load server list

--- settingsDialog.hxx
+++ settingsDialog.hxx
@@ -5,15 +5,15 @@
 #ifndef DB_QT_COURSEWORK_SETTINGSDIALOG_HXX
 #define DB_QT_COURSEWORK_SETTINGSDIALOG_HXX
 
+#include <QCloseEvent>
+#include <QDebug>
 #include <QDialog>
 #include <QGridLayout>
 #include <QLineEdit>
 #include <QListWidget>
 #include <QMessageBox>
-#include <QDebug>
 #include <QPushButton>
 #include <QSettings>
-#include <QCloseEvent>
 #include <QStandardItemModel>
 #include <iostream>
 class SettingsDialog : public QDialog {
@@ -38,12 +38,12 @@ public:
         addServer_btn = new QPushButton(this);
         addServer_btn->setText("Add");
         addServer_btn->setObjectName("add-btn");
-       // addServer_btn->setStyleSheet("background:#0275d8;padding: 4px 25px 4px 20px;color:#ffffff;");
+        // addServer_btn->setStyleSheet("background:#0275d8;padding: 4px 25px 4px 20px;color:#ffffff;");
 
         removeServer_btn = new QPushButton(this);
         removeServer_btn->setText("Remove");
         removeServer_btn->setObjectName("remove-btn");
-       // removeServer_btn->setStyleSheet("background:#d9534f;padding: 4px 25px 4px 20px;color:#ffffff;");
+        // removeServer_btn->setStyleSheet("background:#d9534f;padding: 4px 25px 4px 20px;color:#ffffff;");
 
         layout = new QGridLayout(this);
         layout->addWidget(servers_list, 0, 0, 1, 1);
@@ -57,8 +57,9 @@ public:
         connect(removeServer_btn, &QPushButton::clicked, this, [this]() {
             removeServerFromList();
         });
-        settings=new QSettings("vh","DB_Coursework");
-
+        settings = new QSettings("vh", "DB_Coursework");
+        fillServerList(settings->value("db/servers").toStringList());
+        qDebug()<<settings->value("db/servers").toStringList();
     }
     QGridLayout *layout{};
     QStandardItemModel *itemModel;
@@ -66,33 +67,48 @@ public:
     QLineEdit *ipAddress_inp;
     QPushButton *addServer_btn;
     QPushButton *removeServer_btn;
-    QSettings* settings;
+    QSettings *settings;
 
 
 private:
-       void closeEvent(QCloseEvent *event) override{
-           QMessageBox::StandardButton reply;
-           reply = QMessageBox::question(this, "Test", "Quit?",
-                                         QMessageBox::Yes|QMessageBox::No);
-           if (reply == QMessageBox::Yes) {
-               qDebug() << "Yes was clicked";
-               event->accept();
-               QDialog::closeEvent(event);
-           } else {
-               qDebug() << "Yes was *not* clicked";
-               event->ignore();
-           }
-
+    void closeEvent(QCloseEvent *event) override {
+        QMessageBox::StandardButton reply;
+        reply = QMessageBox::question(this, "Test", "Quit?",
+                                      QMessageBox::Yes | QMessageBox::No);
+        if (reply == QMessageBox::Yes) {
+            qDebug() << "Yes was clicked";
+            saveServerList();
+            event->accept();
+            QDialog::closeEvent(event);
+        } else {
+            qDebug() << "Yes was *not* clicked";
+            event->ignore();
         }
+    }
     void removeServerFromList() const {
         itemModel->removeRow(servers_list->currentIndex().row());
     }
 
     void addServer() const {
-        if(ipAddress_inp->text().isEmpty()){return;}
+        if (ipAddress_inp->text().isEmpty()) { return; }
         auto *newServer = new QStandardItem(ipAddress_inp->text());
         itemModel->appendRow(newServer);
         ipAddress_inp->clear();
+    }
+
+    void fillServerList(const QStringList &loadedServer_list) const {
+        for (auto &srv : loadedServer_list) {
+            auto *newServer = new QStandardItem(srv);
+            itemModel->appendRow(newServer);
+        }
+    }
+    void saveServerList() {
+        QStringList servers_from_list;
+        for (int i = 0; i < itemModel->rowCount(); ++i) {
+            servers_from_list << itemModel->index(i, 0).data().toString();
+        }
+
+        settings->setValue("db/servers", QVariant::fromValue(servers_from_list));
     }
 };
 
