ec06aff55aa9675f70de79b5fea8cf2e65631afa
Vladimir Shubarin <vhundef@gmail.com>
2021-03-25 23:08:55 +0300

    Created serverManager class

---
+++ server_manager.hpp
@@ -0,0 +1,14 @@
+//
+// Created by vhund on 25.03.2021.
+//
+
+#ifndef DB_QT_COURSEWORK__SERVER_MANAGER_HPP_
+#define DB_QT_COURSEWORK__SERVER_MANAGER_HPP_
+
+
+class ServerManager {
+
+};
+
+
+#endif//DB_QT_COURSEWORK__SERVER_MANAGER_HPP_
